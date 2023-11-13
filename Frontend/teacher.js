f35396e6fbae9f9c9fc44ccb2eca4bb8a72a0cc7
vp967128 <80238419+vp967128@users.noreply.github.com>
2023-11-12 18:26:24 -0800

    Update teacher.js

--- Frontend/teacher.js
+++ Frontend/teacher.js
@@ -1,7 +1,7 @@
-function GET(teacher_id) {
+function GET(id) {
     var url = "http://127.0.0.1:5000/teacherCourses/2";
-    // var url = "http://lodocus.pythonanywhere.com/teacherCourses/3";
-    // var url = "http://127.0.0.1:5000/teacherCourses/" + teacher_id;
+    // var url = "http://lodocus.pythonanywhere.com/teacherCourses/" + id;
+    // var url = "http://127.0.0.1:5000/teacherCourses/" + id;
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", url, true);
     xhttp.onload= function() {
@@ -10,7 +10,7 @@ function GET(teacher_id) {
         // document.getElementById("courseinfo").innerHTML += `<td>${rep.display}</td>`
         var i;
         for(i = 0; i < rep.length; i++) {
-            document.getElementById("courseinfo").innerHTML += `<tr><td><a href="grades.html"> ${rep[i].Course} </a></td><td>${rep[i].Teacher}</td><td>${rep[i].Time}</td><td>${rep[i].Capacity}</td></tr>`
+            document.getElementById("courseinfo").innerHTML += `<tr><td><a href="grades.html"> ${rep[i].Course} </a></td><td>${rep[i].Teacher}</td><td>${rep[i].Time}</td><td>${rep[i].EnrolledNum}/${rep[i].Capacity}</td></tr>`
             document.getElementById("namedisplay").innerHTML = `Welcome ${rep[i].Teacher}!`
         }
 
@@ -22,3 +22,4 @@ function GET(teacher_id) {
     };
     xhttp.send();
 } 
+
